0ce0ddcd0b3e7cd9a9f828169e70b6bc0ee1341e
ogadmiral <MustaphaDamouh@outlook.com>
2026-04-21 15:28:08 +0100

    ex03

---
+++ py_bracket_validator.py
@@ -0,0 +1,23 @@
+def transform(character: str) -> str:
+	if character == ')':
+		return '('
+	elif character == '}':
+		return '{'
+	elif character == ']':
+		return '['
+	return ""
+
+def bracket_validator(s: str) -> bool:
+	open = '([{'
+	close = ')]}'
+	stack = []
+	for c in s:
+		if c in open:
+			stack.append(c)
+		elif c in close:
+			if transform(c) == stack[-1]:
+				stack.pop(-1)
+			else:
+				return False
+
+	return True
